ee575b59ab3f18eeaf82d6af501ebd00870b6ac5
piotrec <piotrec@op.pl>
2020-12-06 18:14:10 +0100

    minor correction

--- Home.py
+++ Home.py
@@ -5,7 +5,7 @@ from flask_restful import Resource
 class Home(Resource):
 
     def get(self):
-        response = make_response(render_template(render_template("home.html")))
+        response = make_response(render_template("home.html"))
         response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
         response.headers['Cache-Control'] = 'public, max-age=0'
         return response
