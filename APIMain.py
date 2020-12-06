732fda34aa1b6219c662e1ccf9d4fa2ee8ee1f07
piotrec <piotrec@op.pl>
2020-12-06 18:11:30 +0100

    useless calssses and files removed

--- APIMain.py
+++ APIMain.py
@@ -2,13 +2,10 @@ from AdditionalFunctions import AdditionalFunctions
 from Config import GeneralConfig
 from EuklidesExtendedAlgorithm import EuklidesExtendedAlgorithm
 from EulerFunction import EulerFunction
-from ExampleClass import ExampleClass
 from Factorization import Factorization
 from FermatTest import FermatTest
-from FilesTransportApi import FilesTransportApi
 from flask import Flask
 from flask_restful import Api
-from HashFunctions import HashFunctions
 from Home import Home
 from PrimeNumbers import PrimeNumbers
 from RSADecrypt import RSADecrypt
@@ -23,7 +20,6 @@ app.config["SECRET_KEY"] = app_config.secret_key
 
 api = Api(app)
 """Helpers"""
-HashFunctions = HashFunctions()
 AdditionalFunctions = AdditionalFunctions()
 
 """Api methods"""
@@ -36,10 +32,6 @@ api.add_resource(EuklidesExtendedAlgorithm, '/euklides_extended_algorithm', endp
 api.add_resource(RSAKeysPairGenerate, '/rsa_keys_pair_generate', endpoint='rsa_keys_pair_generate')
 api.add_resource(RSAEncrypt, '/rsa_encrypt', endpoint='rsa_encrypt')
 api.add_resource(RSADecrypt, '/rsa_decrypt', endpoint='rsa_decrypt')
-api.add_resource(FilesTransportApi, '/file/download/<path:file_name>', endpoint='file_download')
-api.add_resource(FilesTransportApi, '/file/upload', endpoint='file_upload')
-api.add_resource(ExampleClass, '/samples', endpoint='samples_get')
-api.add_resource(ExampleClass, '/sample/add', endpoint='sample_add')
 
 if __name__ == "__main__":
     app.run(debug=app_config.debug_mode, host='0.0.0.0')
--- Home.py
+++ Home.py
@@ -9,6 +9,5 @@ class Home(Resource):
         response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
         response.headers['Cache-Control'] = 'public, max-age=0'
         return response
-        return make_response(render_template("home.html"))
 
 
